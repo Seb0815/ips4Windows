4e67dd1758d5238a2820b7df5580d49b598e9766
Seb0815 <sebastian@kutzke.de>
2016-01-18 14:35:02 +0100

    .

--- ips4WinNotifications/module.php
+++ ips4WinNotifications/module.php
@@ -6,8 +6,6 @@
 		private $header2 = 'X-WNS-Type: ';
    		private $header3 = 'Authorization: Bearer ';
    		private $header4 = 'X-WNS-Tag: ';
-   		private $deviceURI;
-    	private $authToken;
     	private $Debug = false;
     	private $WNSMsgToken = "abc";
 
@@ -15,14 +13,7 @@
     	{
 		    // Diese Zeile nicht löschen
             parent::__construct($ObjID);
- 
-			$deviceToken = GetValueString($ObjID);
-			$data = explode("###",$deviceToken);
-			if (count($data) > 1)
-			{
-				$this->deviceURI = $data[0];
-				$this->authToken = $data[1];
-			}
+ 			
     	}
 
 		public function Create() 
@@ -35,9 +26,7 @@
 		public function ApplyChanges() 
 		{ 
 				//Never delete this line! 
- 				parent::ApplyChanges(); 
- 			 
- 				
+ 				parent::ApplyChanges();  				
 		} 
     	
     	public function EnableDebug($Debug)
@@ -52,9 +41,9 @@
 		}
 
 
-		public function sendBagdeNotification($Value)
+		public function sendBagdeNotification($device, $Value)
 	 	{
-	 	   $headers = array($this->header1,$this->header2.'wns/badge',$this->header3.$this->authToken);
+	 	   
 	 	   $body = '<?xml version="1.0" encoding="utf-8"?><badge version="1" value="'.$Value.'"/>';
 	 	   if ($this->Debug)
 	 	   {
@@ -62,45 +51,38 @@
 	 	      print_r($headers);
 	 	      echo "body\n".$body."\n";
 			}
-	 	   $response = $this->sendNotification($headers,$body);
+	 	   $response = $this->sendNotification($device,"Bagde",$body);
 	 	   return $response;
 	 	}
 
-		 public function sendToastNotification($text1,$text2)
-	 	 {
-	 	   $headers = array($this->header1,$this->header2.'wns/toast',$this->header3.$this->authToken);
-	 	   $body = '<?xml version="1.0" encoding="utf-8"?><toast><visual><binding template="ToastText02">'.
-	 	   '<text id="1">'.$text1.'</text><text id="2">'.$text2.'</text>'.
-			'</binding></visual></toast>';
+		public function sendToastNotification($device,$text1,$text2)
+	 	{
 			
-			if ($this->Debug)
-	 	   {
-	 	      echo "headers\n";
-	 	      print_r($headers);
-	 	      echo "body\n".$body."\n";
+	 	    $body = "<?xml version=\"1.0\" encoding=\"utf-8\"?><toast><visual><binding template=\"ToastGeneric\"><text>".$text1."</text><text>".$text2."</text></binding></visual></toast>";
+			
+	    	if ($this->Debug)
+	 	    {
+	 			echo "headers\n";
+	 			print_r($headers);
+	 			echo "body\n".$body."\n";
 			}
-	 	   $response = $this->sendNotification($headers,$body);
-	 	   return $response;
+	 	    $response = $this->sendNotification($device,"Toast",$body);
+	 	    return $response;
 	 	}
-	 	 	public function sendTileNotification($text1,$text2,$text3, $text4)
-	 	 {
+
+		public function sendTileNotification($device, $text1,$text2,$text3)
+	 	{
+			
+	 	  
 			$text1 = utf8_encode($text1);
 			$text2 = utf8_encode($text2);
 			$text3 = utf8_encode($text3);
-			$text4 = utf8_encode($text4);
+			$bodystart = "<tile><visual>";
+			$bodysmall = "<binding template=\"TileSmall\" hint-textStacking=\"center\"><text hint-align=\"center\" hint-overlay=\"30\"><image src=\"ms-appx:///Assets/Icons/Logo44x44.png\" placement=\"peak\"/><text hint-style=\"body\" hint-align=\"center\">".$text1."</text><text hint-style=\"base\" hint-align=\"center\">".$text2."</text></binding>";
+			$bodymedium = "<binding template=\"TileMedium\" hint-textStacking=\"center\"><image src=\"ms-appx:///Assets/Icons/SmallLogo.png\" placement=\"peek\" hint-crop=\"circle\"/><text hint-style=\"base\" hint-align=\"center\">".$text1."</text><text hint-style=\"captionSubtle\" hint-align=\"center\">".$text2."</text></binding>";
+			$bodyend = "</visual></tile>""
 			
-			$body_medium = '<binding template="TileSquare150x150PeekImageAndText03" fallback="TileSquarePeekImageAndText01">'.
-       			  '<image id="1" src="ms-appx:///assets/House-white.100.png" alt="ipsControlImage"/>'.
-       			  '<text id="1">'.$text1.'</text><text id="2">'.$text2.'</text><text id="3">'.$text3.'</text><text id="4">'.$text4.'</text>'.
-     				  '</binding>';
-     		$body_large = '<binding template="TileWide310x150PeekImageAndText02" fallback="TileWidePeekImage02">'.
-       			  '<image id="1" src="ms-appx:///assets/House-white.100.png" alt="ipsControlImage"/>'.
-       			  '<text id="1">'.$text1.'</text><text id="2">'.$text2.'</text><text id="3">'.$text3.'</text><text id="4">'.$text4.'</text>'.
-     				  '</binding>';
-			
-	 	   $headers = array($this->header1,$this->header2.'wns/tile',$this->header4.$this->WNSMsgToken,$this->header3.$this->authToken );
-	 	   $body = '<tile><visual version="2">'.$body_medium.$body_large.'</visual></tile>';
-
+			$body = $bodystart.$bodysmall.$bodymedium.$bodyend;
 
 			if ($this->Debug)
 	 	   {
@@ -108,103 +90,149 @@
 	 	      print_r($headers);
 	 	      echo "body\n".$body."\n";
 			}
-	 	   $response = $this->sendNotification($headers,$body);
+	 	   $response = $this->sendNotification($device, "Tile",$body);
 	 	   return $response;
 	 	}
-	 	
-	 	public function sendTileNotification_gross($text1,$text2,$text3)
-	 	 {
 
-	 	   $headers = array($this->header1,$this->header2.'wns/tile',$this->header4.$this->WNSMsgToken,$this->header3.$this->authToken );
-	 	   $body = '<tile><visual version="3"><binding template="TileWide310x150IconWithBadgeAndText">'.
-	 	      '<image id="1" src="ms-appx:///assets/House-white.100.png" alt="ipsControlImage"/>'.
-	 	   	'<text id="1">'.$text1.'</text><text id="2">'.$text2.'</text><text id="3">'.$text3.'</text>'.
-			'</binding></visual></tile>';
+		private function getDeviceAddress($deviceName)
+		{
+				
+			$ret = IPS_GetInstanceListByModuleID ("{44D8BE09-743E-484F-A64D-154C4235BE94}");
+			$DeviceURL = "";
+			$AccessToken = "";
+			$retArray = "";
+			$SecChannelExp = 0;
 
-			if ($this->Debug)
-	 	   {
-	 	      echo "headers\n";
-	 	      print_r($headers);
-	 	      echo "body\n".$body."\n";
-			}
-	 	   $response = $this->sendNotification($headers,$body);
-	 	   return $response;
-	 	}
-	 	public function sendTileNotification_mittel($text1,$text2,$text3)
-	 	 {
-
-	 	   $headers = array($this->header1,$this->header2.'wns/tile',$this->header4.$this->WNSMsgToken,$this->header3.$this->authToken );
-	 	   $body = '<tile><visual version="3"><binding template="TileSquare150x150IconWithBadge">'.
-	 	      '<image id="1" src="ms-appx:///assets/House-white.100.png" alt="ipsControlImage"/>'.
-	 	   	'</binding></visual></tile>';
-
-			if ($this->Debug)
-	 	   {
-	 	      echo "headers\n";
-	 	      print_r($headers);
-	 	      echo "body\n".$body."\n";
-			}
-	 	   $response = $this->sendNotification($headers,$body);
-	 	   return $response;
-	 	}
-	 	public function sendTileNotification_klein($text1,$text2,$text3)
-	 	 {
-	 	   $headers = array($this->header1,$this->header2.'wns/tile',$this->header4.$this->WNSMsgToken,$this->header3.$this->authToken );
-	 	   $body = '<tile><visual version="3"><binding template="TileSquare71x71IconWithBadge"><image id="1" src="ms-appx:///assets/House-white.100.png" alt="ipsControlImage"/></binding></visual></tile>';
-
-
-			if ($this->Debug)
-	 	   {
-	 	      echo "headers\n";
-	 	      print_r($headers);
-	 	      echo "body\n".$body."\n";
-			}
-	 	   $response = $this->sendNotification($headers,$body);
-	 	   return $response;
-	 	}
-	 	
-	 	private function sendNotification($headers,$body)
-	 	{
-	 	 	if ($this->Debug)
-	 	   {
-	 	      echo "URL:\n".$this->deviceURI."\n";
-			}
-			
-			if ($this->deviceURI != "")
+			if ($ret == null)
+				echo "Error: ips4WinDeviceRegistration is not implemented! Please create a new instance of ips4WinDeviceRegistration!\n";
+			else if (count($ret) > 1)
 			{
-		 		// use Client URL Library
-				$ch = curl_init();
-				// set an options for a cURL transfer
-				// look options here:  http://www.php.net/manual/en/function.curl-setopt.php
-				// and what are needed here: http://msdn.microsoft.com/en-us/library/windowsphone/develop/hh202970(v=vs.92).aspx
-				curl_setopt($ch, CURLOPT_URL, $this->deviceURI);
-				curl_setopt($ch, CURLOPT_POST, true);
-				curl_setopt($ch, CURLOPT_HEADER, true);
-				curl_setopt($ch, CURLOPT_HTTPHEADER,$headers);
-				curl_setopt($ch, CURLOPT_POSTFIELDS, $body);
-				curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-				curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-				// perform a cURL session
-				$response = curl_exec($ch);
-
-				// Check for errors
-				if($response === FALSE)
-				{
-				  echo "Error in ips4WinNotifications->sendNotifications: ".curl_error($ch);
-				}
-				// close a cURL session
-				curl_close($ch);
-				if($response === FALSE)
-					return "Error in ips4WinNotifications->sendNotifications: ".curl_error($ch);
-				else
-			   return true;
+				echo "Error: multiple ips4WinDeviceRegistration found, unsupported installation! Please make sure you have only one instance of ips4WinDeviceRegistration!\n";
 			}
 			else
-				return "Error in ips4WinNotifications->sendNotifications: DeviceURI is null! Will skip message";
-		
+			{
+				$id = $ret[0];
+				$objList = IPS_GetChildrenIDs($id);
+				//print_r($objList);
+				$i = count($objList);
+				for ($j=0;$j<$i;$j++)
+				{
+					$vid = @IPS_GetObjectIDByName ( "deviceName", $objList[$j]);
+					if($vid !== false)
+					{
+						if ($deviceName == null)
+						{
+							$device = GetValueString($vid);
+		         			$vid = @IPS_GetObjectIDByName ( "SecChannel", $objList[$j]);
+							if ($vid !== false)
+								$DeviceURL = GetValueString($vid);
+							
+							$vid = @IPS_GetObjectIDByName ( "AccessToken", $objList[$j]);
+							if ($vid !== false)
+								$AccessToken = GetValueString($vid);
+							
+							$vid = @IPS_GetObjectIDByName ( "SecChannelExp", $objList[$j]);
+							if ($vid !== false)
+								$SecChannelExp = GetValueInteger($vid);
 
-			
+							if ($retArray == "")
+							{
+						  		$retArray[0] = explode("###",$device."###".$DeviceURL."###".$AccessToken."###".$SecChannelExp);
+							}
+							else
+							{
+								$retArray[count($retArray)] = explode("###",$device."###".$DeviceURL."###".$AccessToken."###".$SecChannelExp);
+		         			}
+							$DeviceURL = "";
+							$AccessToken = "";
+							$SecChannelExp = 0;
+						}
+						else
+						{
+							if (strtolower($deviceName) == strtolower(GetValueString($vid)))
+							{
+								$vid = @IPS_GetObjectIDByName ( "SecChannel", $objList[$j]);
+								if ($vid !== false)
+									$DeviceURL = GetValueString($vid);
+								
+								$vid = @IPS_GetObjectIDByName ( "AccessToken", $objList[$j]);
+								if ($vid !== false)
+									$AccessToken = GetValueString($vid);
+								
+								$vid = @IPS_GetObjectIDByName ( "SecChannelExp", $objList[$j]);
+								if ($vid !== false)
+									$SecChannelExp = GetValueInteger($vid);
+
+								$retArray = array(explode("###",$deviceName."###".$DeviceURL."###".$AccessToken."###".$SecChannelExp));
+								$j = $i;
+							}
+						}
+					}
+				}		   
+			}
+			return $retArray;
 		}
+	 	
+	 
+	 	private function sendNotification($device, $MsgType ,$body)
+	 	{
+			$deviceArray = $this->getDeviceAddress($device);
+			
+			$i = count($deviceArray);
+			for ($j=0;$j<$i;$j++)
+			{
+				if ($this->Debug)
+	 			{
+					echo "process ".$MsgType." Message for ".$deviceArray[$j][0]." (Device URL: )".$deviceArray[$j][1]."\n";
+				}
+				$SecChannelExp = false;
+				if ($deviceArray[$j][3] < time())
+					$SecChannelExp = true;
+				if ($deviceArray[$j][1] == "" | $deviceArray[$j][2] == "" | $SecChannelExp)
+				{
+					return "Error in ips4WinNotifications->sendNotifications: skip device ".$deviceArray[$j][0]." device URL/AuthToken is empty or SecureChannel is Expired!\n";
+				}
+				else
+				{
+					$authToken = $deviceArray[$j][2];
+					$headers = "";
+					if ($MsgType == "")
+						$headers = array($this->header1,$this->header2.'wns/badge',$this->header3.$authToken);
+					else if ($MsgType == "Toast")
+						$headers = array($this->header1,$this->header2.'wns/toast',$this->header3.$authToken);				
+					else if ($MsgType == "Tile")
+						$headers = array($this->header1,$this->header2.'wns/tile',$this->header4.$this->WNSMsgToken,$this->header3.$authToken );
 
+					// use Client URL Library
+					$ch = curl_init();
+					// set an options for a cURL transfer
+					// look options here:  http://www.php.net/manual/en/function.curl-setopt.php
+					// and what are needed here: http://msdn.microsoft.com/en-us/library/windowsphone/develop/hh202970(v=vs.92).aspx
+					curl_setopt($ch, CURLOPT_URL, $deviceArray[$j][1]);
+					curl_setopt($ch, CURLOPT_POST, true);
+					curl_setopt($ch, CURLOPT_HEADER, true);
+					curl_setopt($ch, CURLOPT_HTTPHEADER,$headers);
+					curl_setopt($ch, CURLOPT_POSTFIELDS, $body);
+					curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+					curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+					// perform a cURL session
+					$response = curl_exec($ch);
+
+					// Check for errors
+					if($response === FALSE)
+					{
+					  echo "Error in ips4WinNotifications->sendNotifications: ".curl_error($ch);
+					}
+					// close a cURL session
+					curl_close($ch);
+					if($response === FALSE)
+						return "Error in ips4WinNotifications->sendNotifications: ".curl_error($ch);
+					else
+				   return true;
+				}
+
+
+			}					
+		}
 	}
 ?>
